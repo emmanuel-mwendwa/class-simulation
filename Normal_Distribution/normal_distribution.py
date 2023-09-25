78fd36a660fbea0d533a058dd36500665fa458b6
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-09-25 15:46:19 +0300

    Normal_Distribution

---
+++ Normal_Distribution/normal_distribution.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from IPython.display import Math,Latex
+from IPython.core.display import Image
+import seaborn as sns
+
+from scipy.stats import gamma
+data_gamma = gamma.rvs(a=5, size=10000)
+ax = sns.distplot(data_gamma,
+                 kde=True,
+                 bins=100,
+                 color='skyblue',
+                 hist_kws={"linewidth":15, 'alpha': 1})
+ax.set(xlabel='Normal Distribution', ylabel='Frequency')
